d64b5474a3cf0e565b2ad20057e98e144d3e0ed9
unknown <sbicalho86@gmail.com>
2019-08-13 19:26:26 -0300

    uses bootstrap

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request,"hello.html")
+    return render(request,"hello_world.html")
 
 def about_page(request):
     return HttpResponse("<h1>About us</h1>")
