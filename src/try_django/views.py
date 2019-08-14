5ee4e0cf064ea8ae532cdca8399bb258cce215d6
unknown <sbicalho86@gmail.com>
2019-08-14 17:41:16 -0300

    introduces db

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 
 def home_page(request):
     my_title = "Hello there...."
-    return render(request,"hello_world.html",{"title": my_title})
+    context = {"title": my_title}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1,2,3,4,5]}
+    return render(request,"home.html",context)
 
 def about_page(request):
     my_title = "More about us...."
-    return render(request,"hello_world.html",{"title": my_title})
+    return render(request,"about.html",{"title": my_title})
 
 def contact_page(request):
     my_title = "Please contact us...."
