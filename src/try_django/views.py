c4f52f61b28bcc5ac59fee477dc7ffda2bbb6575
unknown <sbicalho86@gmail.com>
2019-08-14 09:32:25 -0300

    rendering title pages

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request,"hello_world.html")
+    my_title = "Hello there...."
+    return render(request,"hello_world.html",{"title": my_title})
 
 def about_page(request):
-    return HttpResponse("<h1>About us</h1>")
+    my_title = "More about us...."
+    return render(request,"hello_world.html",{"title": my_title})
 
 def contact_page(request):
-    return HttpResponse("<h1>Contact us</h1>")
+    my_title = "Please contact us...."
+    return render(request,"hello_world.html",{"title": my_title})
